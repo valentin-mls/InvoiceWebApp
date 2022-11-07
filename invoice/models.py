402af558a9c799376b76511a50709316d6368b06
valentin-mls <valentin.malhas7@gmail.com>
2022-11-07 14:40:27 +0100

    First Commit

---
+++ invoice/models.py
@@ -0,0 +1,205 @@
+from audioop import reverse
+from django.db import models
+from django.template.defaultfilters import slugify
+from django.utils import timezone
+from uuid import uuid4
+from django.contrib.auth.models import User
+from internationalflavor.vat_number import VATNumberField
+from django_countries.fields import CountryField
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Client(models.Model):
+
+    TYPE_CHOICES = (
+    ('Belgian-company','Belgian company'),
+    ('private-person-EU', 'private person within the EU'),
+    ('company-EU','company within the EU'),
+    ('non-EU-customers','non-EU customers'),
+    )
+
+
+    type = models.CharField(max_length=45, choices=TYPE_CHOICES, default='Belgian-company')
+    tva = VATNumberField(default='', blank=True)
+    clientName = models.CharField(default='', max_length=50)
+    adresse = models.CharField(default='', max_length=50)
+    zip_code = models.CharField(("zip code"),default='', max_length=5)
+    country = CountryField(default='', blank_label='(select country)')
+    contact = models.CharField(default="", max_length=50)
+    email = models.EmailField(default="", max_length=254)
+    phone_number = PhoneNumberField(default="")
+    website = models.CharField(default="", max_length=50)
+
+
+    #Utility fields
+    uniqueId = models.CharField(null=True, blank=True, max_length=100)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    date_created = models.DateTimeField(blank=True, null=True)
+    last_updated = models.DateTimeField(blank=True, null=True)
+
+
+    def __str__(self):
+        return '{} {} {}'.format(self.clientName, self.country, self.uniqueId)
+
+
+    def get_absolute_url(self):
+        return reverse('client-detail', kwargs={'slug': self.slug})
+
+
+    def save(self, *args, **kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('-')[4]
+            self.slug = slugify('{} {} {}'.format(self.clientName, self.country, self.uniqueId))
+
+        self.slug = slugify('{} {} {}'.format(self.clientName, self.country, self.uniqueId))
+        self.last_updated = timezone.localtime(timezone.now())
+
+        super(Client, self).save(*args, **kwargs)
+
+
+
+class Invoice(models.Model):
+    TERMS = [
+    ('14 days', '14 days'),
+    ('30 days', '30 days'),
+    ('60 days', '60 days'),
+    ]
+
+    STATUS = [
+    ('CURRENT', 'CURRENT'),
+    ('EMAIL_SENT', 'EMAIL_SENT'),
+    ('OVERDUE', 'OVERDUE'),
+    ('PAID', 'PAID'),
+    ]
+
+    title = models.CharField(null=True, blank=True, max_length=100)
+    number = models.CharField(null=True, blank=True, max_length=100)
+    dueDate = models.DateField(null=True, blank=True)
+    paymentTerms = models.CharField(choices=TERMS, default='14 days', max_length=100)
+    status = models.CharField(choices=STATUS, default='CURRENT', max_length=100)
+    notes = models.TextField(null=True, blank=True)
+
+    #RELATED fields
+    client = models.ForeignKey(Client, blank=True, null=True, on_delete=models.SET_NULL)
+    # product = models.ForeignKey('Product', blank=True, null=True, on_delete=models.CASCADE, related_name='+')
+    
+
+    #Utility fields
+    uniqueId = models.CharField(null=True, blank=True, max_length=100)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    date_created = models.DateTimeField(blank=True, null=True)
+    last_updated = models.DateTimeField(blank=True, null=True)
+
+
+    def __str__(self):
+        return '{} {}'.format(self.number, self.uniqueId)
+
+
+    def get_absolute_url(self):
+        return reverse('invoice-detail', kwargs={'slug': self.slug})
+
+
+    def save(self, *args, **kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('-')[4]
+            self.slug = slugify('{} {}'.format(self.number, self.uniqueId))
+
+        self.slug = slugify('{} {}'.format(self.number, self.uniqueId))
+        self.last_updated = timezone.localtime(timezone.now())
+
+        super(Invoice, self).save(*args, **kwargs)
+
+
+
+class Product(models.Model):
+    CURRENCY = [
+    ('$', 'USD'),
+    ]
+
+    title = models.CharField(null=True, blank=True, max_length=100)
+    description = models.TextField(null=True, blank=True)
+    quantity = models.FloatField(null=True, blank=True)
+    price = models.FloatField(null=True, blank=True)
+    currency = models.CharField(choices=CURRENCY, default='R', max_length=100)
+
+    #Related Fields
+    invoice = models.ForeignKey(Invoice, blank=True, null=True, on_delete=models.CASCADE, related_name='+')
+
+    #Utility fields
+    uniqueId = models.CharField(null=True, blank=True, max_length=100)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    date_created = models.DateTimeField(blank=True, null=True)
+    last_updated = models.DateTimeField(blank=True, null=True)
+
+
+    def __str__(self):
+        return '{} {}'.format(self.title, self.uniqueId)
+
+
+    def get_absolute_url(self):
+        return reverse('product-detail', kwargs={'slug': self.slug})
+
+
+    def save(self, *args, **kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('-')[4]
+            self.slug = slugify('{} {}'.format(self.title, self.uniqueId))
+
+        self.slug = slugify('{} {}'.format(self.title, self.uniqueId))
+        self.last_updated = timezone.localtime(timezone.now())
+
+        super(Product, self).save(*args, **kwargs)
+
+
+
+class Settings(models.Model):
+
+    PROVINCES = [
+    ('Gauteng', 'Gauteng'),
+    ('Free State', 'Free State'),
+    ('Limpopo', 'Limpopo'),
+    ]
+
+    #Basic Fields
+    clientName = models.CharField(null=True, blank=True, max_length=200)
+    clientLogo = models.ImageField(default='default_logo.jpg', upload_to='company_logos')
+    addressLine1 = models.CharField(null=True, blank=True, max_length=200)
+    province = models.CharField(choices=PROVINCES, blank=True, max_length=100)
+    postalCode = models.CharField(null=True, blank=True, max_length=10)
+    phoneNumber = models.CharField(null=True, blank=True, max_length=100)
+    emailAddress = models.CharField(null=True, blank=True, max_length=100)
+    taxNumber = models.CharField(null=True, blank=True, max_length=100)
+
+
+    #Utility fields
+    uniqueId = models.CharField(null=True, blank=True, max_length=100)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    date_created = models.DateTimeField(blank=True, null=True)
+    last_updated = models.DateTimeField(blank=True, null=True)
+
+
+    def __str__(self):
+        return '{} {} {}'.format(self.clientName, self.province, self.uniqueId)
+
+
+    def get_absolute_url(self):
+        return reverse('settings-detail', kwargs={'slug': self.slug})
+
+
+    def save(self, *args, **kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('-')[4]
+            self.slug = slugify('{} {} {}'.format(self.clientName, self.province, self.uniqueId))
+
+        self.slug = slugify('{} {} {}'.format(self.clientName, self.province, self.uniqueId))
+        self.last_updated = timezone.localtime(timezone.now())
+
+        super(Settings, self).save(*args, **kwargs)
---
+++ invoicing/urls.py
@@ -0,0 +1,18 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from invoice import views as invoice_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    #INVOICE URLS
+    path('', invoice_views.index, name='index'),
+    path('invoice/',include('invoice.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
